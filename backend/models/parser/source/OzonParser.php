140eec11b479b6b223cd1c6d8f25f6f03b9b24a9
cocostru <dk.pochtamp@gmail.com>
2018-01-10 01:17:01 +0800

    lazy

--- backend/models/parser/source/OzonParser.php
+++ backend/models/parser/source/OzonParser.php
@@ -107,17 +107,17 @@ class OzonParser extends Parser implements ParserSourceInterface
                             $dataL3[] = $keyLine;
                         }
 
-                        // if ($nest == 4) {
-                        //     $data[$key]['children'][end($dataL1)]['children'][end($dataL2)]['children'][end($dataL3)]['children'][$keyLine] = [
-                        //         'csid'       => '',
-                        //         'dump'       => '',
-                        //         'alias'      => '',
-                        //         'href'       => $expLine[1],
-                        //         'title'      => $title,
-                        //         'nest_level' => 4,
-                        //     ];
-                        //     $dataL4[] = $keyLine;
-                        // }
+                        if ($nest == 4) {
+                            $data[$key]['children'][end($dataL1)]['children'][end($dataL2)]['children'][end($dataL3)]['children'][$keyLine] = [
+                                'csid'       => '',
+                                'dump'       => '',
+                                'alias'      => '',
+                                'href'       => $expLine[1],
+                                'title'      => $title,
+                                'nest_level' => 4,
+                            ];
+                            $dataL4[] = $keyLine;
+                        }
 
                         unset($nest, $keyLine, $line, $expLine, $title);
 
